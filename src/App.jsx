6c08c8d381f7d6ac93bc0e48532f71924399fb68
devid-deshmukh <deviddeshmukh22@gmail.com>
2026-01-20 10:53:11 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./Layouts/MainLayout";
-import './App.css'
 import Home from "./pages/Home";
-import Events from "./pages/Events";
-import Team from "./pages/Team";
 import Join from "./pages/Join";
-import Connect from "./pages/Connect";
 
 function App() {
   return (
@@ -13,14 +9,9 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/events" element={<Events />} />
-          <Route path="/team" element={<Team />} />
           <Route path="/join" element={<Join />} />
-          <Route path="/connect" element={<Connect />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,22 @@
-import Hero from "@/components/Hero";
-import AboutSection from "@/components/AboutSection"; // New unified component
+import Hero from "../components/Hero"; // Your About content is likely in here
+import Events from "./Events";
+import Team from "./Team";
+import Connect from "./Connect";
 
 const Home = () => {
   return (
-    <div className="bg-black">
+    <div className="bg-neutral-950">
+      {/* 1. Hero & About (Wrapped together) */}
       <Hero />
-      <AboutSection />
-      {/* You can add your Join or Events sections below this later */}
+
+      {/* 2. Events Section */}
+      <Events />
+
+      {/* 3. Team Section (The one with the President fix) */}
+      <Team />
+
+      {/* 4. Connect Section */}
+      <Connect />
     </div>
   );
 };
